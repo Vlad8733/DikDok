273a49c785c6cc85f56c68829469a0af8b73e85f
RaivoKinne <120594466+raivokinne@users.noreply.github.com>
2025-01-20 09:41:38 +0200

    SCRUM-3_Update_file_with_new_class_Lietotajs

--- dikdok.php
+++ dikdok.php
@@ -7,7 +7,12 @@ class Video {
     public $url;
     public $author_id;
     public $name;
-
+    /**
+     * @param mixed $id
+     * @param mixed $url
+     * @param mixed $author_id
+     * @param mixed $name
+     */
     public function __construct($id,$url,$author_id,$name) {
         $this->id=$id;
         $this->url=$url;
@@ -18,3 +23,23 @@ class Video {
 }
 
 $video = new Video(1,'https://example.com', 1, 'Dance');
+
+class Lietotajs {
+	public $id;
+	public $lietotajvards;
+	public $epasts;
+	public $parole;
+    /**
+     * @param mixed $id
+     * @param mixed $lietotajvards
+     * @param mixed $epasts
+     * @param mixed $parole
+     */
+    public function __construct($id, $lietotajvards,$epasts,$parole)
+	{
+		$this->id = $id;
+		$this->lietotajvards = $lietotajvards;
+		$this->epasts = $epasts;
+		$this->parole = $parole;
+	}
+}
